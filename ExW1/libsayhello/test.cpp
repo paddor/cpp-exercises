3fcf006dcca6f6af95f8e2185464b6324758e68a
Patrik Wenger <paddor@gmail.com>
2014-10-09 22:22:35 +0200

    test libsayhello

---
+++ ExW1/libsayhello/test.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "sayhello.cpp"
+#include <sstream>
+TEST_CASE("says hello") {
+	auto os = std::ostringstream();
+	sayhello(os);
+	REQUIRE("Hello, world!\n" == os.str());
+}
+
---
+++ ExW1/sayhello_standalone/sayhello.cpp
@@ -0,0 +1,5 @@
+#include "sayhello.hpp"
+#include <ostream>
+void sayhello(std::ostream &out){
+	out << "Hello, world!\n";
+}
