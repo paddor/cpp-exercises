21482dcca9010bbdb8796626fe3c1374e71b2fd1
Patrik Wenger <paddor@gmail.com>
2014-10-11 20:58:07 +0200

    make calculator usable both interactively and non-interactively
    For non-interactive mode, just pass calculation terms as command line
    arguments.

--- calculator/src/main.cpp
+++ calculator/src/main.cpp
@@ -1,10 +1,23 @@
 #include "calc.cpp"
+#include <vector>
+#include <string>
 #include <iostream>
+#include <sstream>
+#include <iterator>
 
-int main(){
-	while (std::cin) {
-		std::cout << "Please type your input term: ";
-		int result = calc(std::cin);
-		std::cout << "= " << result << '\n';
+int main(int argc,char *argv[]){
+	if (argc == 1) {
+		while (std::cin) {
+			std::cout << "Please type your input term: ";
+			int result = calc(std::cin);
+			std::cout << "= " << result << '\n';
+		}
+	} else {
+		std::vector<std::string> arguments(argv, argv+argc);
+		for_each(std::next(arguments.begin()), arguments.end(), [](std::string term) {
+			std::istringstream input { term };
+			auto result = calc(input);
+			std::cout << term << " = " << result << '\n';
+		});
 	}
 }
