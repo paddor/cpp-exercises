58c540e5e7fc66b3a94ffa305b9f8898531c4da3
Patrik Wenger <paddor@gmail.com>
2014-10-11 20:51:11 +0200

    include invalid operator in exception message

--- calc/src/calc.cpp
+++ calc/src/calc.cpp
@@ -1,5 +1,6 @@
 #include "calc.h"
 #include <stdexcept>
+#include <string>
 
 // Calculates the result of two numbers a and b and basic math operator.
 int calc(int a, int b, char operator_symbol) {
@@ -12,8 +13,11 @@ int calc(int a, int b, char operator_symbol) {
 		case '-': return a-b;
 		case '%': return a%b;
 	}
-	throw std::invalid_argument{"invalid operator"};
-}
+
+	auto reason = std::string("invalid operator: ");
+	reason.push_back(operator_symbol);
+	throw std::invalid_argument{reason};
+};
 
 // Just like calc(int, int, char), but gets the numbers and operator
 // from an input stream.
