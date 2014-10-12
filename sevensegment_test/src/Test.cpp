2298b2fa75cd0486e407237c4b7daa006753942b
Patrik Wenger <paddor@gmail.com>
2014-10-12 21:43:06 +0200

    test throwing of std::out_of_range when given number is out of range

--- sevensegment_test/src/Test.cpp
+++ sevensegment_test/src/Test.cpp
@@ -19,10 +19,15 @@ void it_prints_large_digit() {
 	printLargeDigit(8,output);
 	ASSERT_EQUAL(large_8, output.str());
 }
+void it_throws_when_number_out_of_range() {
+	std::ostringstream output {};
+	ASSERT_THROWS(printLargeDigit(10,output), std::out_of_range);
+}
 
 void runAllTests(int argc, char const *argv[]){
 	cute::suite s { };
 	s.push_back(CUTE(it_prints_large_digit));
+	s.push_back(CUTE(it_throws_when_number_out_of_range));
 	cute::xml_file_opener xmlfile(argc,argv);
 	cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
 	cute::makeRunner(lis,argc,argv)(s, "AllTests");
