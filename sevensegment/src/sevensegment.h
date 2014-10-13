e16bf6a567b034a2d11f2324845739a0233542fb
Patrik Wenger <paddor@gmail.com>
2014-10-14 01:11:46 +0200

    remove useless comment

--- sevensegment/src/sevensegment.h
+++ sevensegment/src/sevensegment.h
@@ -7,7 +7,6 @@ namespace sevensegment {
 	// n: scale factor
 	void printLargeDigit(unsigned i, std::ostream &out, unsigned n=1);
 
-	// same for numbers
 	void printLargeNumber(int i, std::ostream &out, unsigned n=1);
 
 	void printLargeError(std::ostream &out, unsigned n=1);
