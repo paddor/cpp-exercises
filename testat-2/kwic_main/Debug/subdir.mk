4e8ab4c0dda31946ed5c87865e9c0caf2a63dec4
Patrik Wenger <paddor@gmail.com>
2014-11-16 22:04:15 +0100

    move workspace

--- testat-2/kwic_main/Debug/subdir.mk
+++ testat-2/kwic_main/Debug/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/Users/paddor/Schule/HSR/14-15/Prog3/workspace_testat_2/word" -I"/Users/paddor/Schule/HSR/14-15/Prog3/workspace_testat_2/kwic/src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I"/Users/paddor/Schule/HSR/14-15/Prog3/workspace/testat-2/word" -I"/Users/paddor/Schule/HSR/14-15/Prog3/workspace/testat-2/kwic/src" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
