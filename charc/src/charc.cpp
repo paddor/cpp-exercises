af4710998939c316feb1a2ae6547518d1b6d1740
Patrik Wenger <paddor@gmail.com>
2015-10-17 00:55:11 +0200

    revert charc to its intended purpose

--- charc/src/charc.cpp
+++ charc/src/charc.cpp
@@ -2,32 +2,15 @@
 #include <iterator>
 #include <algorithm>
 
-
-bool process_character(const char c) {
-	static bool in_word = false;
-
-	if (std::isalnum(c)) {
-		if (! in_word) {
-			in_word = true;
-			return true;
-		}
-	} else {
-		if (in_word) in_word = false;
-	}
-	return false;
-}
-
 int main() {
 	using it = std::istreambuf_iterator<char>;
 //	using it = std::istream_iterator<char>;
-
 //	std::noskipws(std::cin);
-	size_t count = std::count_if(it {std::cin}, it{}, process_character); // {
-//		return true; // count all characters
-//		return std::isalnum(c); // count alphanumeric characters
-//		std::cout << "processing: '" << c << "' (in_word = " << in_word << ")" << std::endl;
 
-//	});
+	size_t count = std::count_if(it {std::cin}, it{}, [](char c) {
+		return true; // count all characters
+//		return std::isalnum(c); // count alphanumeric characters
+	});
 
 	std::cout << count << '\n';
 }
