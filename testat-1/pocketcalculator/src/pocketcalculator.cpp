7bfb40a65ee2e99bcc012e93de5728307edd1a0c
Patrik Wenger <paddor@gmail.com>
2015-10-19 19:59:42 +0200

    compare to nullptr explicitly

--- testat-1/pocketcalculator/src/pocketcalculator.cpp
+++ testat-1/pocketcalculator/src/pocketcalculator.cpp
@@ -20,9 +20,10 @@ namespace pocketcalculator {
 	}
 
 	unsigned preferred_scale() {
-		if (const char* scale_ptr = getenv("POCKETCALCULATOR_SCALE") ) {
+		const char* scale_ptr = getenv("POCKETCALCULATOR_SCALE");
+		if (scale_ptr != nullptr)
 			return std::stoul(std::string { scale_ptr });
-		}
+
 		return 0;
 	}
 }
