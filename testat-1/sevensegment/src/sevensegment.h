1641f60c3f7842e1b00f4131cd73144ecb1dfb3f
Patrik Wenger <paddor@gmail.com>
2015-10-20 03:00:13 +0200

    add TODO

--- testat-1/sevensegment/src/sevensegment.h
+++ testat-1/sevensegment/src/sevensegment.h
@@ -4,6 +4,7 @@
 #include <iosfwd>
 
 namespace sevensegment {
+// TODO: why default params?
 	void printLargeDigit(const unsigned i, std::ostream& out, const unsigned scale_factor=1);
 	void printLargeNumber(const int i, std::ostream& out, const unsigned scale_factor=1);
 	void printLargeError(std::ostream& out, const unsigned scale_factor=1);
