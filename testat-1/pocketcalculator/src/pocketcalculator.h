bd6146eda6a2e907a3de433c3036d2e357770444
Patrik Wenger <paddor@gmail.com>
2015-10-20 03:16:16 +0200

    add TODO

--- testat-1/pocketcalculator/src/pocketcalculator.h
+++ testat-1/pocketcalculator/src/pocketcalculator.h
@@ -4,7 +4,7 @@
 #include <iosfwd>
 
 namespace pocketcalculator {
-	void start(std::istream& input, std::ostream& output, const unsigned n = 2);
+	void start(std::istream& input, std::ostream& output, const unsigned n = 2); // TODO: rename n=>scale_factor
 	unsigned preferred_scale();
 }
 
